c874fd02d2f2e5a2af656b56625114aecc477f79
郑书法 <10257469+zheng-calligraphy@user.noreply.gitee.com>
2022-11-16 08:26:52 +0000

    update src/main/java/com/sculling/sculling/ScullingApplication.java.
    Signed-off-by: 郑书法 <10257469+zheng-calligraphy@user.noreply.gitee.com>

--- src/main/java/com/sculling/sculling/ScullingApplication.java
+++ src/main/java/com/sculling/sculling/ScullingApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ScullingApplication {
 
     public static void main(String[] args) {
