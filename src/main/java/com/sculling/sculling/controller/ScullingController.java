48c9f60ef4467db9db463db353897cfc62a5307b
郑书法 <10257469+zheng-calligraphy@user.noreply.gitee.com>
2022-11-16 08:30:37 +0000

    update src/main/java/com/sculling/sculling/controller/ScullingController.java.
    Signed-off-by: 郑书法 <10257469+zheng-calligraphy@user.noreply.gitee.com>

--- src/main/java/com/sculling/sculling/controller/ScullingController.java
+++ src/main/java/com/sculling/sculling/controller/ScullingController.java
@@ -6,21 +6,20 @@ import com.sculling.sculling.service.ScullingService;
 import com.sculling.sculling.util.RSAUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @Slf4j
-@RefreshScope
+
 public class ScullingController {
 
 
 
-    @Value("${data: 123}")
-    public String data;
+ 
 
     @Autowired
     private ScullingService scullingService;
@@ -39,9 +38,5 @@ public class ScullingController {
         return RSAUtils.encryptByPublicKey(msg.toString());
     }
 
-    @GetMapping("get")
-    public String get(){
-        System.out.println(data);
-        return "data";
-    }
+
 }
