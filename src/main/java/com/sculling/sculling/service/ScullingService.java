1f9343e782693d0d0e25171c155bcd89ffa4e6e9
zhengshufa <1773766682@qq.com>
2022-11-30 09:32:18 +0800

    pus

--- src/main/java/com/sculling/sculling/service/ScullingService.java
+++ src/main/java/com/sculling/sculling/service/ScullingService.java
@@ -98,7 +98,7 @@ public class ScullingService {
                 log.info(baseUrl + URLDecoder.decode(data));
                 Connection c = Jsoup.connect(baseUrl + URLDecoder.decode(data));
                 Document d = c.get();
-                Elements es = d.getElementsByTag("dd").tagName("a").tagName("a");
+                Elements es = d.getElementsByTag("dd").tagName("a");
                 log.info("----",es.outerHtml());
                 log.info("===",es.tagName("a"));
                 urlList.clear();
